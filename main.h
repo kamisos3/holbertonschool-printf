666d20079139f3606487b0c9eba9f1104048e963
kamisos3 <kamilasostre11@gmail.com>
2025-03-26 11:45:03 -0400

    Making function helper putchar

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
-#ifndef MAIN
-#define MAIN
+#include <stdarg.h>
+#include <unistd.h>
 
+#ifndef MAIN_H
+#define MAIN_H
+/*Helper documents and prototype being used*/
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_string(char *str);
 
 #endif
