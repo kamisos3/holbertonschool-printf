b4ebe92bed85faa4f8c35185b32ba50b6e32f72f
kamisos3 <kamilasostre11@gmail.com>
2025-03-29 14:51:37 -0400

    Verifying if null byte works and adding print_number.c

--- main.h
+++ main.h
@@ -8,5 +8,6 @@ int _printf(const char *format, ...);
 int print_string(char *str);
 int _putchar(char c);
 int handle_format(char format, va_list args);
+int print_number(int num);
 
 #endif /* MAIN_H */
---
+++ print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - For number inputs
+ * @num: integer, number to print 
+ * 
+ *
+ * Return: Number of characters printed
+ */
+int print_number(int num)
+{
+	int count = 0;
+
+	if (num < 0)
+	{
+		count += _putchar('-');
+		num = -num;/*Makes num positive*/
+	}
+	if (num / 10) /*For higher digits*/
+		count += print_number(num / 10);
+	count += _putchar((num % 10) + '0');
+
+	return (count);
+}
